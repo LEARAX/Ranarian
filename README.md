d21714a59c01f9c98071f50a1831b99d88ded124
LEARAX57H <willculhane3@gmail.com>
2017-11-11 10:26:46 -0700

    Basic things

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Ranarian-Depletory
-WIP
+# Ranarian
+Ranarian is a discordgo bot. Who knows what it'll do?
+
+## Functions
+Functions/properties of this bot will be listed below as they are added.
+
+## Executing
+To build Ranarian, run the following in your favorite shell:
+```sh
+go get github.com/bwmarrin/discordgo
+go build -o Ranarian main.go
+```
+
+To run Ranarian, execute `./Ranarian -t <TOKEN>` with a token from (the discord developer page)[https://discordapp.com/developers/applications/me].
--- main.go
+++ main.go
@@ -16,13 +16,11 @@ var (
 )
 
 func init() {
-
   flag.StringVar(&Token, "t", "", "Bot Token")
   flag.Parse()
 }
 
 func main() {
-
   // Create a new Discord session using the provided bot token.
   dg, err := discordgo.New("Bot " + Token)
   if err != nil {
@@ -41,7 +39,7 @@ func main() {
   }
 
   // Wait here until CTRL-C or other term signal is received.
-  fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+  fmt.Println("Ranarian online!")
   sc := make(chan os.Signal, 1)
   signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
   <-sc
@@ -53,7 +51,6 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
   // Ignore all messages created by the bot itself
   // This isn't required in this specific example but it's a good practice.
   if m.Author.ID == s.State.User.ID {
