bfe9bf956724cb99d4672ada871bd4166a3c6fc8
LEARAX57H <willculhane3@gmail.com>
2017-12-08 17:31:20 -0700

    Basic elements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 extern crate serenity;
 
+use serenity::prelude::*;
+use serenity::model::*;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn on_message(&self, _: Context, msg: Message) {
+        if msg.content == "!messageme" {
+            if let Err(why) = msg.author.dm(|m| m.content("Hello!")) {
+                println!("Error when direct messaging user: {:?}", why);
+            }
+        }
+    }
+
+    fn on_ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
 
 fn main() {
-    let alpha = "5";
-    println!("{}", alpha)
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+    let mut client = Client::new(&token, Handler);
+
+    if let Err(why) = client.start() {
+        println!("Client error: {:?}", why);
+    }
 }
