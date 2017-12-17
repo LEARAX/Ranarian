b77f0d741bd1152253e70a049dcd579637bf44cd
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-12-16 17:19:42 -0700

    Make the bot join a voice channel

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ extern crate serenity;
 
 use serenity::prelude::*;
 use serenity::model::*;
+use serenity::voice;
 use std::env;
 
-struct Handler;
+struct Handler {
+    target_voice_channel: (u64, u64)
+}
 
 impl EventHandler for Handler {
     fn on_message(&self, _: Context, msg: Message) {
@@ -15,8 +18,11 @@ impl EventHandler for Handler {
         }
     }
 
-    fn on_ready(&self, _: Context, ready: Ready) {
+    fn on_ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+
+        let mut shard = ctx.shard.lock();
+        shard.manager.join(GuildId(self.target_voice_channel.0), ChannelId(self.target_voice_channel.1));
     }
 }
 
@@ -24,7 +30,16 @@ fn main() {
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
-    let mut client = Client::new(&token, Handler);
+    let target_voice_guild = env::var("TARGET_VOICE_GUILD")
+        .expect("Expected a target voice guild ID");
+    let target_voice_channel = env::var("TARGET_VOICE_CHANNEL")
+        .expect("Expected a target voice channel ID");
+    let mut client = Client::new(&token, Handler {
+        target_voice_channel: (
+                                  target_voice_guild.parse::<u64>().expect("Expected guild ID to be numeric"),
+                                  target_voice_channel.parse::<u64>().expect("Expected channel ID to be numeric")
+                              )
+    });
 
     if let Err(why) = client.start() {
         println!("Client error: {:?}", why);
